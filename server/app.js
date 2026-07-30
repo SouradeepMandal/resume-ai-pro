b7ee332bcf52f055e8dd45bcb7c09b55cb54e0f9
Souradeep Mandal <souradeepmandal459@gmail.com>
2026-07-31 02:58:10 +0530

    APIs Done

--- server/app.js
+++ server/app.js
@@ -2,11 +2,17 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Authentication Routes
+app.use("/api/auth", authRoutes);
+
+// Root Route
 app.get("/", (req, res) => {
   res.json({
     message: "ResumeAI Pro Backend Running 🚀",
